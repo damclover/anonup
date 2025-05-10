165e1989ff6cf5c7fde31926bc41c3289453ec6f
DamClover <114771411+damclover@users.noreply.github.com>
2025-05-09 21:14:18 -0300

    Update anonup.py

--- anonup.py
+++ anonup.py
@@ -1,11 +1,55 @@
 #!/usr/bin/env python3
 
+from bs4 import BeautifulSoup
 import argparse
 import requests
 import sys
 
-def upload_file(file_path):
+def upload_anonymous(file_path, use_tor=False):
+    url = 'https://anonymousfiles.org/'
+    proxies = {
+        'http': 'socks5h://127.0.0.1:9050',
+        'https': 'socks5h://127.0.0.1:9050'
+    } if use_tor else None
+
+    try:
+        with open(file_path, 'rb') as f:
+            files = {
+                'file': (file_path, f),
+            }
+            data = {
+                'upload_nonce_field': '0200b150b0',
+                '_wp_http_referer': '/',
+            }
+
+            res = requests.post(url, data=data, files=files, proxies=proxies)
+            soup = BeautifulSoup(res.text, 'html.parser')
+            down_link = soup.find('link', rel='canonical')
+
+            if res.status_code in [200, 302]:
+                if down_link:
+                    link = down_link.get('href')
+                    if use_tor:
+                        print("\n[+] Uploaded in anonymousfiles.org using Tor Proxy")
+                        print(f"[+] Link: {link}")
+                    else:
+                        print("\n[+] Uploaded in anonymousfiles.org")
+                        print(f"[+] Link: {link}")
+                else:
+                    print("[!] Error: No download link found in the response.")
+            else:
+                print(f"[!] Failed upload with status code: {res.status_code}")
+
+    except FileNotFoundError:
+        print(f"[!] File '{file_path}' not found.")
+        sys.exit(1)
+
+def upload_anonup(file_path, use_tor=False):
     url = 'https://www.anonfile.la/process/upload_chunk'
+    proxies = {
+        'http': 'socks5h://127.0.0.1:9050',
+        'https': 'socks5h://127.0.0.1:9050'
+    } if use_tor else None
 
     headers = {
         'Host': 'www.anonfile.la',
@@ -20,7 +64,7 @@ def upload_file(file_path):
         with open(file_path, 'rb') as f:
             file_content = f.read()
     except FileNotFoundError:
-        print(f"[!] Arquivo '{file_path}' não encontrado.")
+        print(f"[!] File '{file_path}' not found.")
         sys.exit(1)
 
     files = {
@@ -35,13 +79,17 @@ def upload_file(file_path):
         'file_size': str(len(file_content)),
     }
 
-    response = requests.post(url, headers=headers, files=files, data=data)
+    response = requests.post(url, headers=headers, files=files, data=data, proxies=proxies)
 
     if response.status_code == 200:
         result = response.json()
         if result.get('success'):
-            print(f"[+] Uploaded")
-            print(f"[+] Link: {result.get('url')}")
+            if use_tor:
+                print("\n[+] Uploaded in anonymousfiles.org using Tor Proxy")
+                print(f"[+] Link: {result.get('url')}")
+            else:
+                print("\n[+] Uploaded in anonymousfiles.org")
+                print(f"[+] Link: {result.get('url')}")
         else:
             print("[!] Error in upload:", result.get('message'))
     else:
@@ -49,14 +97,23 @@ def upload_file(file_path):
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Anonfile.la uploader - Made by DamClover@proton.me",
-        epilog="e.g. python3 anon.py file.txt"
+        description="Anonymous uploader - Made by DamClover@proton.me",
+        epilog="e.g. python3 anon.py file.txt -s 2 --tor"
     )
     parser.add_argument('file', help='File to upload')
+    parser.add_argument('-s', '--system', type=int, choices=[1, 2], help='System to upload (e.g. 1: anonfile.la, 2: anonymousfiles.org)')
+    parser.add_argument('--tor', action='store_true', help='Use Tor proxy for more anonymity (enable tor on your system)')
 
     args = parser.parse_args()
 
-    upload_file(args.file)
+    if args.system == 1:
+        upload_anonup(args.file, args.tor)
+        
+    elif args.system == 2:
+        upload_anonymous(args.file, args.tor)
+        
+    else:
+        upload_anonup(args.file, args.tor)
 
 if __name__ == "__main__":
     main()
